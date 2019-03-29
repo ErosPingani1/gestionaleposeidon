5caa9a236b39af6d6ba7b72c33d3f23fa358cad9
luca sica <f.581luca@gmail.com>
2019-03-29 15:42:42 +0100

    Update UtentiController.java

--- poseidon/src/main/java/it/academy/be/controller/UtentiController.java
+++ poseidon/src/main/java/it/academy/be/controller/UtentiController.java
@@ -1,5 +1,7 @@
 package it.academy.be.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +20,40 @@ public class UtentiController {
 	private UtentiService service;
 	
 	@RequestMapping(path= "/ricercaNome", produces="application/json")
-	public Cliente ricercaNome(@RequestParam("q") String query) throws Exception {
+	public Cliente ricercaNome(@RequestParam("nome") String query) throws Exception {
 		
+		System.out.println("sono passato");
 		return findByName(query);
 	}
 	
-	//Metodo findByName che viene collegato al medesimo metodo del Service
+	@GetMapping(path= "/ricercaCompleta", produces="application/json")
+	public List<Cliente> ricercaCompleta() throws Exception {
+		
+		return findAll();
+	}
+	
+	@RequestMapping(path= "/ricercaPagina", produces="application/json")
+	public List<Cliente> ricercaPagina(@RequestParam("pagina") int pagina, @RequestParam("i") int itemPerPagina) throws Exception {
+		
+		return findPaged(pagina, itemPerPagina);
+	}
+	
+	//Metodo findByName che viene collegato al medesimo metodo del Service degli utenti
 	private Cliente findByName(String query) throws DBException {
 		
 		return service.findByName(query);
+	}
+	
+	//Metodo findAll() che viene collegato al metodo findUtenti del Service degli utenti
+	private List<Cliente> findAll() throws DBException {
+		
+		return service.findUtenti();
+	}
+	
+	//Metodo findAll() che viene collegato al metodo findUtenti con i parametri pagine del service degli utenti
+	private List<Cliente> findPaged(int pagina, int itemPerPagina) throws DBException {
+		
+		return service.findUtenti(pagina, itemPerPagina);
 	}
 
 }
