e5de067b9b7c69e483ef39543e4fb5545300ed73
luca sica <f.581luca@gmail.com>
2019-03-29 15:43:04 +0100

    Update AffittoService.java

--- poseidon/src/main/java/it/academy/be/service/AffittoService.java
+++ poseidon/src/main/java/it/academy/be/service/AffittoService.java
@@ -1,5 +1,25 @@
 package it.academy.be.service;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.academy.be.dao.AffittoDao;
+import it.academy.be.dto.Affitto;
+import it.academy.be.exception.DBException;
+
+@Service
 public class AffittoService {
+	@Autowired
+	AffittoDao dao;
+
+	public ArrayList<Affitto> affittiById(int idutente) throws DBException {
+		return dao.selectAffittiById(idutente);
+	}
+
+	public ArrayList<Affitto> getAffittiPendenti() throws DBException {
+		return dao.getAllAffittiinCorso();
+	}
 	
 }
