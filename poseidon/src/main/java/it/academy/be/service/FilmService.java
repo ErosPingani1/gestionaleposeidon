2e0910906cb9f1038e35a517c399bf7617340191
luca sica <f.581luca@gmail.com>
2019-03-29 15:40:47 +0100

    Update FilmService.java

--- poseidon/src/main/java/it/academy/be/service/FilmService.java
+++ poseidon/src/main/java/it/academy/be/service/FilmService.java
@@ -2,32 +2,32 @@ package it.academy.be.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import it.academy.be.dao.FilmDao;
 import it.academy.be.dto.Film;
 import it.academy.be.exception.DBException;
 
+@Service
 public class FilmService {
 
-	private FilmDao dao = new FilmDao();
-	
-	public List<Film> findFilm(int pagina, int itemPerPagina) throws DBException {
-		
-		List<Film> list = dao.findAll();
-		
-		int totElementi = list.size();
-		int fromIndex = -1;
-		int toIndex = -1;
-		
-		fromIndex = Math.min(totElementi, (pagina - 1) * itemPerPagina);
-		System.out.println(fromIndex);
-		toIndex = Math.min(totElementi, fromIndex + itemPerPagina);
-		System.out.println(toIndex);
-		
-		return list.subList(fromIndex, toIndex);
-	}
-	
+	@Autowired
+	private FilmDao dao;
+
 	public List<Film> findFilm() throws DBException {
 		return dao.findAll();
 	}
 	
+	public Film findFilmRichiesto(String titolo){
+		
+		return dao.findFilm(titolo);
+	}
+	
+	public List<Film> findFilmAffittabili(){
+		
+		return dao.findFilmDisponibili();
+		
+	}
+
 }
